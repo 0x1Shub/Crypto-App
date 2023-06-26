f558f1f0d541c781e9c07fb9861b770f027f0814
Shubham <vu1f2021041@pvppcoe.ac.in>
2023-06-27 01:11:23 +0530

    Add: Added Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchanges";
+import CoinDetails from "./components/CoinDetails";
 
 function App() {
   return (
-    <div className="App">
-      Shubham
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/coins" element={<Coins />}/>
+        <Route path="/exchanges" element={<Exchanges />}/>
+        <Route path="/coin/:id" element={<CoinDetails />}/>
+      </Routes>
+    </Router>
   );
 }
 
